e32708591a3ee275b88bf54be9d8516cc5b61750
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-02-15 21:32:55 -0800

    fixBug: can't mjs for preload.js, should be js

--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -1,9 +1,7 @@
-// const { contextBridge, ipcRenderer } = require('electron');
-import { contextBridge, ipcRenderer } from 'electron';
+const { contextBridge, ipcRenderer } = require('electron');
 
 console.log("✅ Preload.js is running!"); // Debugging log
 
 contextBridge.exposeInMainWorld('electronAPI', {
   scheduleZoom: (timeSheet, zoomLink) => ipcRenderer.send('schedule-zoom', timeSheet, zoomLink)
 });
-
