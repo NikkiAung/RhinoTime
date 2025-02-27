983ef5c22c241b6b902701b877531c6a5795c2c2
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-02-26 18:01:25 -0800

    create ecosystem.config.js for ec2 AWS deployment

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.export = {
+    apps: [
+      {
+        name: "project-management",
+        script: "npm",
+        args: "run dev",
+        env: {
+          NODE_ENV: "development"
+        }
+      }
+    ]
+}
+  
